d5484ed45fa1c85fe64a8c49fa4c852ba83327d1
Arthur DressenWall <arthurdanieldw@gmail.com>
2020-12-19 23:12:20 -0600

    I apparently forgot to push these earlier!

--- robotSensors/task2.py
+++ robotSensors/task2.py
@@ -5,6 +5,7 @@ from sklearn import tree
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import *
 import random
+import math
 
 
 def readCSV(file):
@@ -21,14 +22,14 @@ if __name__ == "__main__":
     sensorTrainData, sensorTestData, sensorTrainTarget, sensorTestTarget = train_test_split(sensorData, sensorTarget, test_size=.1)
 
     # Decision trees
-    dTree = tree.DecisionTreeClassifier(splitter="random", )
+    dTree = tree.DecisionTreeClassifier()
     dTree.fit(sensorTrainData, sensorTrainTarget)
     print("Example decision tree prediction:", dTree.predict(sensorTestData[0:1]))
     print("Example decision tree target", sensorTestTarget[0])
 
     # MLP (Multilayer Perceptrons)
     mlp = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                        hidden_layer_sizes=(5,5,5,5,5, 2), random_state=1, max_iter=200, activation = "tanh")
+                        hidden_layer_sizes=(5, 2), random_state=1, max_iter=200)
     mlp.fit(sensorTrainData, sensorTrainTarget)
     print("Example neural network prediction:", mlp.predict(sensorTestData[0:1]))
     print("Example neural network target", sensorTestTarget[0])
@@ -44,21 +45,22 @@ if __name__ == "__main__":
 
     print("Classification reports for testing data:")
     print(classification_report(sensorTestTarget, dTreeTestPredictions))
-    # print(classification_report(sensorTestTarget, mlpTestPredictions))
+    print(classification_report(sensorTestTarget, mlpTestPredictions))
 
     print("Confusion matrices for training data:")
     print(confusion_matrix(sensorTrainTarget, dTreeTrainPredictions))
-    # print(confusion_matrix(sensorTrainTarget, mlpTrainPredictions))
+    print(confusion_matrix(sensorTrainTarget, mlpTrainPredictions))
 
     print("Confusion matrices for testing data:")
     print(confusion_matrix(sensorTestTarget, dTreeTestPredictions))
-    # print(confusion_matrix(sensorTestTarget, mlpTestPredictions))
+    print(confusion_matrix(sensorTestTarget, mlpTestPredictions))
 
 
     print("Cross validation:")
     random.seed()
-    cv = KFold(n_splits=10,shuffle=True)
+    cv = KFold(n_splits=10,shuffle=False)
     cvTree = cross_val_score(dTree, sensorData, sensorTarget, cv=cv)
     cvMLP = cross_val_score(mlp, sensorData, sensorTarget, cv=cv)
     print(cvTree, np.mean(cvTree))
     print(cvMLP, np.mean(cvMLP))
+
